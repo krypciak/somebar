d46c50faab2fc278a8972ba3e8b6c1e0e90e1160
krypek <115574014+krypciak@users.noreply.github.com>
2022-11-20 13:57:34 +0100

    Adjust font size

--- src/config.hpp
+++ src/config.hpp
@@ -6,11 +6,11 @@
 
 constexpr bool topbar = true;
 
-constexpr int paddingX = 10;
+constexpr int paddingX = 8;
 constexpr int paddingY = 3;
 
 // See https://docs.gtk.org/Pango/type_func.FontDescription.from_string.html
-constexpr const char* font = "Sans 12";
+constexpr const char* font = "Sans 11";
 
 constexpr ColorScheme colorInactive = {
     /* tag_fg:      */ Color(0xbb, 0xbb, 0xbb),
@@ -29,8 +29,8 @@ constexpr ColorScheme colorActive = {
     /* titlebar_bg: */ Color(0x22, 0x22, 0x22),
     /* layout_fg:   */ Color(0xbb, 0xbb, 0xbb),
     /* layout_bg:   */ Color(0x22, 0x22, 0x22),
-    /* status_fg:   */ Color(0x22, 0x22, 0x22),
-    /* status_bg:   */ Color(0x22, 0x22, 0x22),
+    /* titlebar_fg: */ Color(0xdd, 0xdd, 0xdd),
+    /* titlebar_bg: */ Color(0x22, 0x22, 0x22),
 };
 constexpr const char* termcmd[] = {"alacritty", nullptr};
 
