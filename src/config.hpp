90995c34b6e268ff11bae0b27ab11d18ce99f9f2
krypek <115574014+krypciak@users.noreply.github.com>
2022-11-20 13:54:07 +0100

    Add more theming options

--- src/config.hpp
+++ src/config.hpp
@@ -12,8 +12,26 @@ constexpr int paddingY = 3;
 // See https://docs.gtk.org/Pango/type_func.FontDescription.from_string.html
 constexpr const char* font = "Sans 12";
 
-constexpr ColorScheme colorInactive = {Color(0xbb, 0xbb, 0xbb), Color(0x22, 0x22, 0x22)};
-constexpr ColorScheme colorActive = {Color(0xee, 0xee, 0xee), Color(0x00, 0x55, 0x77)};
+constexpr ColorScheme colorInactive = {
+    /* tag_fg:      */ Color(0xbb, 0xbb, 0xbb),
+    /* tag_bg:      */ Color(0x22, 0x22, 0x22),
+    /* titlebar_fg: */ Color(0xbb, 0xbb, 0xbb),
+    /* titlebar_bg: */ Color(0x22, 0x22, 0x22),
+    /* layout_fg:   */ Color(0xbb, 0xbb, 0xbb),
+    /* layout_bg:   */ Color(0x22, 0x22, 0x22),
+    /* status_fg:   */ Color(0x22, 0x22, 0x22),
+    /* status_bg:   */ Color(0x22, 0x22, 0x22),
+};
+constexpr ColorScheme colorActive = {
+    /* tag_fg:      */ Color(0xee, 0xee, 0xee),
+    /* tag_bg:      */ Color(0x00, 0x55, 0x77),
+    /* titlebar_fg: */ Color(0xdd, 0xdd, 0xdd),
+    /* titlebar_bg: */ Color(0x22, 0x22, 0x22),
+    /* layout_fg:   */ Color(0xbb, 0xbb, 0xbb),
+    /* layout_bg:   */ Color(0x22, 0x22, 0x22),
+    /* status_fg:   */ Color(0x22, 0x22, 0x22),
+    /* status_bg:   */ Color(0x22, 0x22, 0x22),
+};
 constexpr const char* termcmd[] = {"alacritty", nullptr};
 
 static std::vector<std::string> tagNames = {
